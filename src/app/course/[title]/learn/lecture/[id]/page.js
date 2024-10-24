a93c3cf98672471aa21a08679eb02e5598a9d753
mohran777 <zharmo72@gmail.com>
2024-10-24 21:39:35 +0000

    add to local

---
+++ src/app/course/[title]/learn/lecture/[id]/page.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+
+      <div className="w-full mx-auto top-0 bg-[#2D2F31] text-white shadow-sm">
+        <div className="px-4 py-1">
+          <div>
+            <h1 className="font-bold bg-black text-base">dddddddd</h1>
+          </div>
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default page
