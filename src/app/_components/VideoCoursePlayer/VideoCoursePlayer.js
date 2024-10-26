911a993dbdf2beed31f8484624edf31cfee0f169
mohran777 <zharmo72@gmail.com>
2024-10-26 19:49:10 +0000

    lasts updates

---
+++ src/app/_components/VideoCoursePlayer/VideoCoursePlayer.js
@@ -0,0 +1,22 @@
+"use client"
+// import { CldVideoPlayer } from 'next-cloudinary';
+import React from 'react';
+import dynamic from 'next/dynamic';
+const CldVideoPlayer = dynamic( () => import('next-cloudinary').then((m)=>m.CldVideoPlayer), { ssr: false });
+const VideoCoursePlayer = () => {
+    return (
+        <>
+
+            <CldVideoPlayer
+                width="1920"
+                height="1080"
+                src="https://res.cloudinary.com/djtjlvuvb/video/upload/v1729369464/cbx06ulgjz9vadhd90md.mp4"
+            />
+
+        </>
+    );
+};
+
+export default VideoCoursePlayer;
+
+
--- src/app/course/[title]/learn/lecture/[id]/page.js
+++ src/app/course/[title]/learn/lecture/[id]/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 const page = () => {
   return (
     <>
-
+  
       <div className="w-full mx-auto top-0 bg-[#2D2F31] text-white shadow-sm">
         <div className="px-4 py-1">
           <div>
