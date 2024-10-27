4bb234027fe5763f1ec1a4833e4fc6da821af04e
mohran777 <zharmo72@gmail.com>
2024-10-27 20:41:14 +0000

    add the updates before codespace stop

--- src/app/_components/TabsCourseView/TabsCourseView.js
+++ src/app/_components/TabsCourseView/TabsCourseView.js
@@ -4,10 +4,15 @@ import { FaStar } from "react-icons/fa";
 import { BsPatchExclamationFill } from "react-icons/bs";
 import Rating from '../Rating/Rating';
 import { Progress } from '@/components/ui/progress';
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { IoSearchSharp } from "react-icons/io5";
+
+
 
 const TabsCourseView = () => {
     return (
-        <Tabs defaultValue="overview" className="w-full">
+        <Tabs defaultValue="reviews" className="w-full">
             <TabsList>
                 <TabsTrigger value="content" className="md:hidden" >content</TabsTrigger>
                 <TabsTrigger value="overview" className="text-base focus:font-semibold">Overview</TabsTrigger>
@@ -19,21 +24,21 @@ const TabsCourseView = () => {
                     <h3 className='text-2xl font-semibold'> Spring Boot 3: Learn Spring 6, Spring Core, Spring REST, Spring MVC, Spring Security, Thymeleaf, JPA, Hibernate, MySQL</h3>
 
                     <div className="flex items-center justify-start mt-9 gap-7">
-                            <div className="flex items-start  flex-col">
-                                <div className="flex "> <p className='text-[#4D3105] text-lg font-bold mr-2 '>4.8</p> <div className='mt-1 text-[#B4690E]'><FaStar /></div></div>
-                                <div><p className='text-sm	font-thin	text-slate-500'>85,415 ratings</p></div>
-                            </div>
-                            <div className="flex items-start  flex-col">
-                                <div className="flex "> <p className='text-[#3B3D3F] text-lg font-bold mr-2 '>404,348</p> </div>
-                                <div><p className='text-sm	font-thin	text-slate-500'>Students</p></div>
-                            </div>
-                            <div className="flex items-start flex-col">
-                                <div className="flex "> <p className='text-[#3B3D3F] text-lg font-bold mr-2 '>33.5 hours</p> </div>
-                                <div><p className='text-sm	font-thin	text-slate-500'>Total</p></div>
-                            </div>
+                        <div className="flex items-start  flex-col">
+                            <div className="flex "> <p className='text-[#4D3105] text-lg font-bold mr-2 '>4.8</p> <div className='mt-1 text-[#B4690E]'><FaStar /></div></div>
+                            <div><p className='text-sm	font-thin	text-slate-500'>85,415 ratings</p></div>
+                        </div>
+                        <div className="flex items-start  flex-col">
+                            <div className="flex "> <p className='text-[#3B3D3F] text-lg font-bold mr-2 '>404,348</p> </div>
+                            <div><p className='text-sm	font-thin	text-slate-500'>Students</p></div>
+                        </div>
+                        <div className="flex items-start flex-col">
+                            <div className="flex "> <p className='text-[#3B3D3F] text-lg font-bold mr-2 '>33.5 hours</p> </div>
+                            <div><p className='text-sm	font-thin	text-slate-500'>Total</p></div>
+                        </div>
                     </div>
                     <div className='flex mt-11'>
-                       <div className="mt-1"><BsPatchExclamationFill /></div>   <p className="ml-5 font-thin"> Last updated September 2024 </p>
+                        <div className="mt-1"><BsPatchExclamationFill /></div>   <p className="ml-5 font-thin"> Last updated September 2024 </p>
 
                     </div>
                 </div>
@@ -47,18 +52,23 @@ const TabsCourseView = () => {
                             <h2 className="text-7xl text-[#B4690E] font-extrabold">
                                 4.6
                             </h2>
-                            <Rating  ratingValue={4.6} readOnly={true} />
+                            <Rating ratingValue={4.6} readOnly={true} />
                             <p className='text-base font-semibold text-[#B4690E]'>Course Rating</p>
                         </div>
                         <div className="flex items-center flex-col gap-5 ">
                             <div className='flex gap-12'> <div><Progress value={62} className="w-[500px]" /></div> <div className='flex'> <Rating className="mb" ratingValue={5} readOnly={true} /> <p className='text-sm ml-4 text-indigo-600'> 62% </p> </div>  </div>
                             <div className='flex gap-12'> <div><Progress value={30} className="w-[500px]" /></div> <div className='flex'> <Rating className="mb" ratingValue={4} readOnly={true} /> <p className='text-sm ml-4 text-indigo-600'> 62% </p> </div>  </div>
-                            <div className='flex gap-12'> <div><Progress value={6}  className="w-[500px]" /></div> <div className='flex'> <Rating className="mb" ratingValue={3} readOnly={true} /> <p className='text-sm ml-4 text-indigo-600'> 62% </p> </div>  </div>
-                            <div className='flex gap-12'> <div><Progress value={1}  className="w-[500px]" /></div> <div className='flex'> <Rating className="mb" ratingValue={2} readOnly={true} /> <p className='text-sm ml-4 text-indigo-600'> 62% </p> </div>  </div>
-                            <div className='flex gap-12'> <div><Progress value={1}  className="w-[500px]" /></div> <div className='flex'> <Rating className="mb" ratingValue={1} readOnly={true} /> <p className='text-sm ml-4 text-indigo-600'> 62% </p> </div>  </div>
+                            <div className='flex gap-12'> <div><Progress value={6} className="w-[500px]" /></div> <div className='flex'> <Rating className="mb" ratingValue={3} readOnly={true} /> <p className='text-sm ml-4 text-indigo-600'> 62% </p> </div>  </div>
+                            <div className='flex gap-12'> <div><Progress value={1} className="w-[500px]" /></div> <div className='flex'> <Rating className="mb" ratingValue={2} readOnly={true} /> <p className='text-sm ml-4 text-indigo-600'> 62% </p> </div>  </div>
+                            <div className='flex gap-12'> <div><Progress value={1} className="w-[500px]" /></div> <div className='flex'> <Rating className="mb" ratingValue={1} readOnly={true} /> <p className='text-sm ml-4 text-indigo-600'> 62% </p> </div>  </div>
                         </div>
                     </div>
                     <h2 className='text-2xl font-extrabold mt-16'>Reviews</h2>
+                    <div className="flex w-full max-w-md items-center mt-5 ">
+                        <Input type="text" placeholder="Search Reviews" />
+                        <Button type="submit"><IoSearchSharp /></Button>
+                        
+                    </div>
                 </div>
 
             </TabsContent>
