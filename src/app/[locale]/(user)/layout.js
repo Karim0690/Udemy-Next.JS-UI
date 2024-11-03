6aa9c81d45faa02ff47afcd04ff16eefac57864e
FatahChan <ahmadfathallah89@gmail.com>
2024-11-03 03:16:21 +0200

    fix: added instructor footer

--- src/app/[locale]/(user)/layout.js
+++ src/app/[locale]/(user)/layout.js
@@ -1,14 +1,15 @@
 import Header from "@/app/_components/Header/Header";
+import InstructorFooter from "../(instructor)/_components/instractorFooter/InstructorFooter";
 
 export default async function RootLayout({ children, params: { locale } }) {
   return (
     <>
       <Header locale={locale} />
       {children}
-      {/* 
+
       <div>
         <InstructorFooter />
-      </div> */}
+      </div>
     </>
   );
 }
